6f773bd54433c46e8877947a6a6263bb7674af6c
Nicolas Le Jeune <nicolasyvonlejeune@gmail.com>
2022-12-20 16:46:13 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Apartments from './pages/Apartments'
-
+import Header from './components/Header';
+import About from './pages/About';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      <Header />
       <Routes>
-        <Route exact path="/" element={<Home />}/>
+        <Route path="/" element={<Home />}/>
         <Route path="/apartments" element={<Apartments />}/>
+        <Route path="/about" element={<About />}/>
+
       </Routes>
     </Router>
   </React.StrictMode>
