e4d9f482714dbcc39a9b3600042737fc29928062
Nicolas Le Jeune <nicolasyvonlejeune@gmail.com>
2023-01-05 19:44:04 +0100

    commit

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -3,25 +3,25 @@ import chevronDown from '../assets/chevron-down.png'
 import chevronUp from '../assets/chevron-up.png'
 import styles from '../styles/Collapse.module.css'
 
-function Collapse(props){
-    const [isOpen, setIsOpen] = useState(false)
- 
-    return isOpen ? (
-        <div className={styles.collapse}>
-            <div className={styles.header}>
-                <h2 className={styles.title}>{props.title}</h2>
-                <img className={styles.chevronUp} src={chevronUp} onClick={() => setIsOpen(false)} alt="open"/>
-            </div>
-            <div className={styles.content}>{props.content}</div>
+function Collapse(props) {
+    const [isOpen, setIsOpen] = useState(false);
+   
+    return (
+      <div className={styles.collapse}>
+        <div className={styles.header}>
+          <h2 className={styles.title}>{props.title}</h2>
+          {isOpen ? (
+            <img className={styles.chevronUp} src={chevronUp} onClick={() => setIsOpen(false)} alt="open" />
+          ) : (
+            <img className={styles.chevronDown} src={chevronDown} onClick={() => setIsOpen(true)} alt="close" />
+          )}
         </div>
-    ) : (
-        <div className={styles.collapse}>
-            <div className={styles.header}>
-                <h2 className={styles.title}>{props.title}</h2>
-                <img className={styles.chevronDown} src={chevronDown} onClick={() => setIsOpen(true)} alt="close"/>
-            </div>
-        </div>
-    )
+        {isOpen && (
+          <div className={styles.content}>{props.content}</div>
+        )}
+      </div>
+    );
 }
+  
 
 export default Collapse
